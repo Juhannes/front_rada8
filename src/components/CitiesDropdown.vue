6dadf0f751e04d55de101e8316479568ca6f81e7
Juhannes <pstimees@gmail.com>
2023-02-21 14:30:34 +0200

    Dropdownide WIP

--- src/components/CitiesDropdown.vue
+++ src/components/CitiesDropdown.vue
@@ -4,6 +4,7 @@
     <option v-if="isSearch" value="0">Kõik linnad</option>
     <option v-else value="0" disabled>Asukoht</option>
     <option v-for="cities in cities" :value="cities.id">{{ cities.name }}</option>
+<!--    <option v-if="isSearch" v-for="advertisements in advertisements" :value="advertisements.cityId">{{ advertisements.cityId }}</option>-->
 
   </select>
 </template>
@@ -17,6 +18,12 @@ export default {
   data: function () {
     return {
       cities: [
+        {
+          id: 0,
+          name: ''
+        }
+      ],
+      availableCities: [
         {
           id: 0,
           name: ''
--- src/views/AdvertisementsView.vue
+++ src/views/AdvertisementsView.vue
@@ -7,7 +7,15 @@
         </div>
         <CitiesDropdown :is-search="isSearch" :advertisements="advertisements"/>
         <br>
+        <select v-model="advertisements.cityName" class="form-select" aria-label="Default select example">
+          <option value="0">Kõik linnad</option>
+          <option v-for="advertisements in advertisements" :value="advertisements.cityName">
+            {{advertisements.cityName}}
+          </option>
+
+        </select>
         <br>
+        <button v-on:click="setCityNames" type="button" class="btn btn-primary">Test</button>
         <br>
         <div>
           Otsin:
@@ -30,7 +38,6 @@ export default {
   components: {AdvertisementsPiano, CitiesDropdown, TypeDropdown},
   mounted() {
     this.callMethodsInPiano()
-
   },
   data: function () {
     return {
@@ -47,6 +54,17 @@ export default {
           modifiedTimestamp: null,
           status: '',
           picture: null
+        }
+      ],
+      cities: [
+        {
+          id: 0,
+          name: ''
+        }
+      ],
+      availableCities: [
+        {
+          name: ''
         }
       ],
       isNewMessage: false,
@@ -99,20 +117,39 @@ export default {
       }).catch(error => {
         console.log(error)
       })
-    }
-    ,
+    },
+    getAllCities: function () {
+      this.$http.get("/cities")
+          .then(response => {
+            this.cities = response.data;
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    },
     openMessageWindow: function (advertisementId) {
       this.isNewMessage = true
       let advertisement = this.advertisements.find(advertisement => advertisement.id === advertisementId)
       this.outGoingMessage.subject = advertisement.header;
       this.getAdvertisementOwner(advertisement.userId);
       this.outGoingMessage.advertisementId = advertisementId;
+    },
+
+    setCityNames: function () {
+      for (let i = 0; i < this.advertisements.length; i++) {
+        for (let j = 0; j < this.cities.length; j++)
+          if (this.advertisements[i].cityId === this.cities[j].id) {
+            this.advertisements[i].cityName = this.cities[j].name
+            console.log(this.cities[j].name)
+          }
+      }
     }
-    ,
   }
   ,
   beforeMount() {
     this.getAllAdvertisements()
+    this.getAllCities()
+    this.setCityNames()
   }
 }
 
